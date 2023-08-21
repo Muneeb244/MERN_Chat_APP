b10540e39f5e061cc47ee3216fed6efd200fed9b
Muneeb Ahmad <91079744+Muneeb244@users.noreply.github.com>
2023-08-21 18:09:46 +0500

    fixed autoscroll bug

--- client/src/components/MessageForm.js
+++ client/src/components/MessageForm.js
@@ -46,33 +46,34 @@ function MessageForm() {
         setMessage("");
     }
 
-
+    
     return (
         <div className='w-full h-fit'>
-            <div className='w-full h-[80vh] border-gray border-[2px] mt-1'>
+            <div className='w-full h-[80vh] border-gray border-[2px] mt-1 overflow-auto'>
                 {!user && <div className='flex items-center w-full h-10 px-5'>Please Login</div>}
                 {user && messages.map(({ _id: date, messagesByDate }, index) => {
-                    return <div key={index} className='w-full h-[80vh] text-center mt-2 bg-gray-300'>
+                    return <div key={index} className='w-full h-[80vh] text-center mt-2 bg-gray-300 z'>
                         <p>{date}</p>
-                        {messagesByDate?.map(({ content, time, from: sender }, msgIndex) => {
-                            return <div className='bg-yellow-300 mt-5 w-1/2'>
-                                    <img className='w-[40px] h-[40px] rounded-full mr-3' src={sender.user.image} alt="" />
-                                    <p>{sender.user._id === user.user._id ? sender.user.name : "You"}</p>
-                                    <p>{content}</p>
-                                    <p>{time}</p>
+                        {messagesByDate?.map(({content, time, from: sender}, msgIndex)=> {
+                            return <div className=' w-1/2 mb-2' key={msgIndex}>
+                                <div className='bg-yellow-300'>
+                                <img className='w-[40px] h-[40px] rounded-full mr-3' src={sender.user.image} alt="" />
+                                <p>{sender.user._id === user.user._id ?  sender.user.name : "You"}</p>
+                                <p>{content}</p>
+                                <p>{time}</p>
                                 </div>
-                            // </div>
+                            </div>
                         })}
                     </div>
                 })}
+            <div ref={messageEndRef}></div>
             </div>
             <div className='flex items-center justify-around p-2'>
                 <input type='text' placeholder='your message' value={message} onChange={e => setMessage(e.target.value)} className='bg-white appearance-none border-gray-200 rounded w-11/12 p-2 text-gray-700 leading-tight focus:outline-none border-gray border-2 focus:border-gray-500' />
-                <button className='flex justify-center items-center w-[40px] h-[40px] bg-yellow-400 rounded hover:bg-yellow-500' disabled={!user} onClick={message !== "" ? handleSubmit : () => { }}>
+                <button className='flex justify-center items-center w-[40px] h-[40px] bg-yellow-400 rounded hover:bg-yellow-500' disabled={!user} onClick={message !== "" ? handleSubmit: () => {} }>
                     <BiSolidPaperPlane color='#fff' size={20} className='' />
                 </button>
             </div>
-            <div ref={messageEndRef}></div>
         </div>
     )
 }
